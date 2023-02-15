add48f0a53575db4a525f375dd30345d724ea8c0
Yann <hardcreative@hotmail.fr>
2023-02-15 13:46:41 +0100

    fsfsf

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   devServer: {
     port: 3030, // you can change the port
     compress: true,
-    allowedHosts: "auto" | "all",
+    allowedHosts: "all",
   },
   module: {
     rules: [
